e006f550686ad30f7ee548dd97ccaaa21f6a3ec9
Tryno <toussaint.quentin00@gmail.com>
2025-01-23 14:35:12 +0100

    Add product update methods and validation for SKU/EAN updates

--- product.py
+++ product.py
@@ -37,3 +37,22 @@ class Product(APIWrapper):
 
     def delete_product(self, product_id):
         return self.delete(f"{self.endpoint}/{product_id}")
+
+    def update_product(self, product_id):
+        return self.put(f"{self.endpoint}/{product_id}")
+
+    def update_product_sku_ean(self, sku = None,  ean13 = None):
+        url_compose = []
+        if sku is None and ean13 is None:
+            raise ValueError("sku or ean13 is required")
+        elif sku is not None and ean13 is not None:
+            raise ValueError("Can't request with both key")
+        elif sku is not None and ean13 is None:
+            url_compose.append("sku")
+            url_compose.append(sku)
+            pass
+        elif ean13 is not None and sku is None:
+            url_compose.append("ean13")
+            url_compose.append(ean13)
+            pass
+        return self.put(f"{self.endpoint}/{url_compose[0]}/{url_compose[1]}", payload)
