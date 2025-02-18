52ff1f2260e448b3bd23c71385e24f8a8666360d
Tryno <toussaint.quentin00@gmail.com>
2025-02-18 11:55:50 +0100

    Refactor Client class to enhance token management; add token refreshing method and update authentication flow

--- shippingboapy/client.py
+++ shippingboapy/client.py
@@ -15,47 +15,72 @@ class Client:
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
+        self.access_token = None
+        self.refresh_token = None
 
 
-        def build_headers(self):
-            """
-            Construit les headers pour les requêtes
-            """
-            return {'Accept': 'application/json', 'X-API-VERSION': f'{self.api_version}', 'X-API-APP-ID': f'{self.app_id}', 'Authorization': f'Bearer {self.access_token}'}
+    def build_headers(self):
+        """
+        Construit les headers pour les requêtes
+        """
+        return {'Accept': 'application/json', 'X-API-VERSION': f'{self.api_version}', 'X-API-APP-ID': f'{self.app_id}', 'Authorization': f'Bearer {self.access_token}'}
         
+    def refreshing_token(self):
+        """
+        Rafraîchit le token
+        """
+        url = f"https://stoplight.io/mocks/shippingbo/api/256683485/oauth/token"
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        payload = {
+            "grant_type": "refresh_token",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "refresh_token": self.refresh_token
+        }
 
-        def run(self, token):
-            """
-            Lance l'authentification
-            """
-            url = f"https://auth.shippingbo.com/oauth/token"
-            headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
-            payload = {
-                "grant_type": "client_credentials",
-                "client_id": self.client_id,
-                "client_secret": self.client_secret,
-                "code": token,
-                "redirect_uri": self.redirect_uri
-            }
+        response = requests.post(url=url, json=payload, headers=headers)
 
-            response = requests.post(url=url, json=payload, headers=headers)
+        match response.status_code:
+            case 200:
+                self.access_token = response.json().get("access_token")
+                self.refresh_token = response.json().get("refresh_token")
 
-            match response.status_code:
-                case 200:
-                    self.access_token = response.json().get("access_token")
-                    self.refresh_token = response.json().get("refresh_token")
-                    self.token_type = response.json().get("token_type")
-                    print("Authentification réussie")
-                case 401:
-                    print("Non autorisé")
-                case 500:
-                    print("Erreur interne du serveur")
-                case 404:
-                    print("Ressource non trouvée")
-                case _:
-                    print("Authentification échouée")
-                    print(response.json())
-                    
+    def run(self, token):
+        """
+        Lance l'authentification
+        """
+        url = f"https://stoplight.io/mocks/shippingbo/api/256683485/oauth/token"
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        payload = {
+            "grant_type": "authorization_code",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "code": token,
+            "redirect_uri": self.redirect_uri
+        }
+
+        response = requests.post(url=url, json=payload, headers=headers)
+
+        match response.status_code:
+            case 200:
+                self.access_token = response.json().get("access_token")
+                self.refresh_token = response.json().get("refresh_token")
+                self.token_type = response.json().get("token_type")
+                print("Authentification réussie")
+            case 401:
+                print("Non autorisé")
+            case 500:
+                print("Erreur interne du serveur")
+            case 404:
+                print("Ressource non trouvée")
+            case _:
+                print("Authentification échouée")
+                print(response.json())
+        print(response.json())                    
+        return response.json()            
--- test_client.py
+++ test_client.py
@@ -5,9 +5,9 @@ app_id = 447
 api_version = 1
 client_id = 'Q8TGUF0QDZVPlDZzck9kIjCs6aj7efqWoNAcnyiljKc'
 client_secret = 'TalkAlPu47s331DxTo-z1btOiww_2luCQ-6w5oc4lX0'
-redirect_uri = 'https://www.shippingbo.com'
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 
 
 if __name__ == "__main__":
-    client = Client(app_id, api_version, client_id, client_secret)
-    client.run("fP_lcOYEyDk5GVpR1EnOYmlCzYi3khbV1DsDcjci864")
+    client = Client(app_id, api_version, client_id, client_secret, redirect_uri)
+    client.run("tSeQNbKlMn0HfyEdHVALr3L-sOMhCNjwJ-H4sDtADUc")
