28fa854c32f450acbbf665fb5f6c7977c2b26c51
Tryno <toussaint.quentin00@gmail.com>
2025-03-04 17:22:39 +0100

    Add RequestError exception class and enhance error handling in Product class methods; improve docstrings for product retrieval methods

--- shippingboapy/product.py
+++ shippingboapy/product.py
@@ -1,4 +1,5 @@
 from .api_wrapper import APIWrapper
+from .exceptions import *
 
 class Product(APIWrapper):
     def __init__(self,client, token):
@@ -8,6 +9,24 @@ class Product(APIWrapper):
         self.access_token = token
         
     def get_products(self, limit:str="10", is_pack:bool=False, ean13:str=None, offset:str=None, updated_at:str=None, user_ref:str=None, sort_id:int=None, sort_updated_at:int="asc"):
+        """
+        Retrieve the list of products.
+        Args:
+            limit (str): The maximum number of products to retrieve. Default is "10".
+            is_pack (bool): Filter to retrieve only pack products. Default is False.
+            ean13 (str, optional): Filter by EAN-13 barcode.
+            offset (str, optional): The offset for pagination.
+            updated_at (str, optional): Filter products updated after this date.
+            user_ref (str, optional): Filter by user reference.
+            sort_id (int, optional): Sort by product ID.
+            sort_updated_at (str): Sort by updated_at field, either "asc" or "desc". Default is "asc".
+        Returns:
+            list: A list of ProductObject instances representing the retrieved products.
+        Raises:
+            ValueError: If sort_updated_at is not "asc" or "desc".
+        Notes:
+            Ensure the client is running with a valid token before calling this method.
+        """
         if self.client.running == False:
             print("Please run client with valid token before refreshing")
             return
@@ -30,6 +49,13 @@ class Product(APIWrapper):
                     product_list.append(ProductObject(product))
                 return product_list
     def get_product_by_id(self, product_id):
+        """
+        Retrieve a product by ID.
+        Args:
+            product_id (str): The ID of the product to retrieve.
+        Raise:
+
+        """
         if self.client.running == False:
             print("Please run client with valid token before refreshing")
             return
@@ -41,13 +67,13 @@ class Product(APIWrapper):
                 case 200:
                     return ProductObject(response.json()['product'])
                 case 404:
-                    raise Exception(f"{response.status_code} Product not found")
+                    raise RequestError(response.status_code,"Product not found")
                 case 403:
-                    raise Exception(f"{response.status_code} Forbidden")
+                    raise RequestError(response.status_code, "Forbidden")
                 case 401:
-                    raise Exception(f"{response.status_code} Unauthorized")
+                    raise RequestError(response.status_code,"Unauthorized")
                 case _:
-                    raise Exception(f"{response.status_code} An error occured")
+                    raise RequestError(response.status_code, "An error occured")
     def build_headers(self):
         self.headers = {
             "Accept": "application/json",
