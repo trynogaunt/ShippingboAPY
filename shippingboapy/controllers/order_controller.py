d54e150f1354fc12730041827302e49a89af567c
trynogaunt <toussaint.quentin00@gmail.com>
2025-04-22 17:09:41 +0200

    Update launch configuration and enhance OrderController and Order model with validation and error handling

--- shippingboapy/controllers/order_controller.py
+++ shippingboapy/controllers/order_controller.py
@@ -27,6 +27,19 @@ class OrderController(AbstractController):
         :param params: Optional parameters to filter the orders.
         :return: A list of Order objects.
         """
+        order_list = []
+        if limit > 50:
+            raise ValueError("Limit parameter must be less than or equal to 50.")
+        if offset < 0:
+            raise ValueError("Offset parameter must be greater than or equal to 0.")
+        if not isinstance(limit, int):
+            raise ValueError("Limit parameter must be an integer.")
+        if not isinstance(offset, int):
+            raise ValueError("Offset parameter must be an integer.")
+        if not isinstance(sort, str):
+            raise ValueError("Sort parameter must be a string.")
+        if not isinstance(kwargs, dict):
+            raise ValueError("Filter parameters must be a dictionary.")
         if sort not in ["asc", "desc"]:
             raise ValueError("Sort parameter must be 'asc' or 'desc'.")
         
@@ -43,5 +56,24 @@ class OrderController(AbstractController):
         response = self._get(endpoint, params=params)
         
         if response:
-            return [Order(order) for order in response['orders']]
-        return []
+            for order in response['orders']:
+                try:
+                    order = self.get_by_id(order['id'])
+                    if order:
+                        order_list.append(order)
+                    else:
+                        print(f"Order with ID {order['id']} not found.")
+                except KeyError as e:
+                    print(f"Key error: {e} in order {order['id']}")
+                    continue
+                except TypeError as e:
+                    print(f"Type error: {e} in order {order['id']}")
+                    continue
+                except ValueError as e:
+                    print(f"Value error: {e} in order {order['id']}")
+                    continue
+                except Exception as e:
+                    print(f"Error retrieving order {order['id']}: {e}")
+                    continue
+            return order_list
+        
--- shippingboapy/models/order_model.py
+++ shippingboapy/models/order_model.py
@@ -1,10 +1,11 @@
 from .abstract_model import AbstractModel
 from .address_model import Address
-
+import datetime
+from datetime import datetime as dt
 class Config:
     def __init__(self, response):
         self.__attributes = []
-        data = response.get("config")
+        data = response
         if data:
             for key, value in data.items():
                 self.__attributes.append(key)
@@ -16,7 +17,7 @@ class Config:
 class ExternalComputedCarrierService:
     def __init__(self, response):
         self.__attributes = []
-        data = response.get("external_computed_carrier_service")
+        data = response
         if data:
             for key, value in data.items():
                 if key == "config":
@@ -27,7 +28,7 @@ class ExternalComputedCarrierService:
 class MappedProducts:
     def __init__(self, response):
         self.__attributes = []
-        data = response.get("mapped_product")
+        data = response
         if data:
             for key, value in data.items():
                 self.__attributes.append(key)
@@ -36,12 +37,41 @@ class MappedProducts:
 class OrderItems:
     def __init__(self, response):
         self.__attributes = []
-        data = response.get("order_item")
+        data = response
         if data:
             for key, value in data.items():
                 self.__attributes.append(key)
                 setattr(self, key, value)
 
+class OrderTags:
+    def __init__(self, response):
+        self.__attributes = []
+        data = response
+        if data:
+            for key, value in data.items():
+                self.__attributes.append(key)
+                setattr(self, key, value)
+
+class OrderDocuments:
+    def __init__(self, response):
+        self.__attributes = []
+        data = response
+        if data:
+            for key, value in data.items():
+                self.__attributes.append(key)
+                setattr(self, key, value)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}> {self.__dict__}>"
+
+class ItemsShipmentSerialNumber:
+    def __init__(self, response):
+        self.__attributes = []
+        data = response
+        if data:
+            for key, value in data.items():
+                self.__attributes.append(key)
+                setattr(self, key, value)
 class Order:
     """
     Product class to represent a product in the ShippingBo API.
@@ -59,13 +89,24 @@ class Order:
         data = response.get("order")
         if data:
             for key, value in data.items():
-                if key == "billing_address" or key == "shipping_address":
-                    value = Address(value)
-                if key == "external_computed_carrier_service":
-                    value = ExternalComputedCarrierService(value)
-                if key == "mapped_products":
-                    value = MappedProducts(value)
-                if key == "order_items":
-                    value = OrderItems(value)
-                self.__attributes.append(key)
+                if value is None:
+                    value = None
+                else:
+                    if key == "billing_address" or key == "shipping_address":
+                        value = Address(value)
+                    if key == "external_computed_carrier_service" and value:
+                        value = ExternalComputedCarrierService(value)
+                    if key == "mapped_products":
+                        value = [MappedProducts(item) for item in value]
+                    if key == "order_items":
+                        value = [OrderItems(item) for item in value]
+                    if key == "order_tags":
+                        value = [OrderTags(item) for item in value]
+                    if key == "shipped_at" or key == "created_at" or key == "updated_at":
+                        value = dt.fromisoformat(value).strftime("%Y-%m-%d %H:%M:%S")
+                    if key == "order_documents":
+                        value = [OrderDocuments(item) for item in value]
+                    if key == "items_shipment_serial_numbers":
+                        value = [ItemsShipmentSerialNumber(item) for item in value]
+                self.__attributes.append(key)    
                 setattr(self, key, value)
