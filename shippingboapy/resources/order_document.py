7b62ca61cb9713ccca9153a9636906c786a7790a
trynogaunt <toussaint.quentin00@gmail.com>
2026-06-25 17:00:16 +0200

    Refactor resource classes to inherit from base resource classes; streamline implementation and remove unused methods

--- shippingboapy/resources/order_document.py
+++ shippingboapy/resources/order_document.py
@@ -1,62 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from shippingboapy.resources.base_resource import Creatable, Gettable, Downloadable
 from shippingboapy.models.order_document import OrderDocument, OrderDocumentCreate
-if TYPE_CHECKING:
-    from shippingboapy.client import Client
     
-class OrderDocumentResource:
-    def __init__(self, client: Client):
-        self.client = client
-    
-    async def get(self, document_id: int, **kwargs) -> OrderDocument:
-        """
-        Get the details of a specific order document by its ID.
-
-        Args:
-            document_id (str): The unique identifier of the order document to retrieve.
-
-        Returns:
-            OrderDocument: An OrderDocument object representing the details of the specified order document.
-        """
-        
-        data = await self.client._request("GET", f"/order_documents/{document_id}", **kwargs)
-        
-        if data is None:
-            return None
-        
-        return OrderDocument.model_validate(data)
-    
-    async def get_file(self, document_id: int, **kwargs) -> bytes:
-        """
-        Get the file content of a specific order document by its ID.
-
-        Args:
-            document_id (str): The unique identifier of the order document to retrieve.
-
-        Returns:
-            bytes: The binary content of the order document file.
-        """
-        
-        data = await self.client._download("GET", f"/order_documents/{document_id}/file", **kwargs)
-        
-        if data is None:
-            return None
-        
-        return data
-    
-    async def create(self, order_document_create: OrderDocumentCreate, **kwargs) -> bool:
-        """
-        Create a new order document for a specific order.
-
-        Args:
-            order_document_create (OrderDocumentCreate): An OrderDocumentCreate object containing the details of the order document to create.
-            
-        Returns:
-            OrderDocument: An OrderDocument object representing the details of the created order document.
-        """
-        data = await self.client._request("POST", "/order_documents", json=order_document_create.model_dump(exclude_none=True, by_alias=True), **kwargs)
-        
-        if data is None:
-            return False
-        
-        return True
+class OrderDocumentResource(Gettable[OrderDocument], Creatable[OrderDocumentCreate, OrderDocument], Downloadable):
+    _path = "order_documents"
+    _model = OrderDocument
--- shippingboapy/resources/order_event.py
+++ shippingboapy/resources/order_event.py
@@ -1,45 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from shippingboapy.resources.base_resource import Gettable, Creatable
 from shippingboapy.models.order_event import OrderEvent, OrderEventCreate
-if TYPE_CHECKING:
-    from shippingboapy.client import Client
     
-class OrderEventResource:
-    def __init__(self, client: Client):
-        self.client = client
-    
-    async def get(self, order_event_id: int, **kwargs) -> OrderEvent:
-        """
-        Get the details of a specific order event by its ID.
-
-        Args:
-            order_event_id (int): The unique identifier of the order event to retrieve.
-
-        Returns:
-            OrderEvent: An OrderEvent object representing the details of the specified order event.
-        """
-        
-        data = await self.client._request("GET", f"/order_events/{order_event_id}", **kwargs)
-        
-        if data is None:
-            return None
-        
-        return OrderEvent.model_validate(data)
-    
-    async def create(self, order_event_create: OrderEventCreate, **kwargs) -> OrderEvent:
-        """
-        Create a new order event for a specific order.
-
-        Args:
-            order_event_create (OrderEventCreate): An OrderEventCreate object containing the details of the order event to create.
-            
-        Returns:
-            OrderEvent: An OrderEvent object representing the newly created order event.
-        """
-        
-        data = await self.client._request("POST", "/order_events", json=order_event_create.model_dump(exclude_none=True, by_alias=True), **kwargs)
-        
-        if data is None:
-            return None
-        
-        return OrderEvent.model_validate(data)
+class OrderEventResource(Gettable[OrderEvent], Creatable[OrderEventCreate, OrderEvent]):
+    _path = "order_events"
+    _model = OrderEvent
