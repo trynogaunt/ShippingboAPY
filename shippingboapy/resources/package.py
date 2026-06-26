62a431a8bf4d3feaa94345be8082e2e995dec9bf
trynogaunt <toussaint.quentin00@gmail.com>
2026-06-26 14:55:32 +0200

    Refactor PackComponentResource and PackageResource to inherit from base resource classes for improved structure and maintainability

--- shippingboapy/resources/package.py
+++ shippingboapy/resources/package.py
@@ -1,27 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from shippingboapy.resources.base_resource import Gettable, Updatable
 from shippingboapy.models.package import Package
-if TYPE_CHECKING:
-    from shippingboapy.client import Client
 
-class PackageResource:
-    def __init__(self, client: Client):
-        self.client = client
-    
-    async def get(self, package_id: int, **kwargs) -> Package:
-        """
-        Retrieve a specific package by its unique identifier.
-
-        Args:
-            package_id (int): The unique identifier of the package to retrieve.
-
-        Returns:
-            Package: The retrieved package.
-        """
-        data = await self.client._request("GET", f"/packages/{package_id}", **kwargs)
-        
-        if data is None:
-            return None
-        
-        return Package.model_validate(data)
-    
+class PackageResource(Gettable[Package], Updatable[Package]):
+    _path = "packages"
+    _model = Package
